34b0c9a41fb675113a38c2d1cc234090e287300a
Lyedson <lyedsonduarte@gmail.com>
2025-05-26 15:47:02 -0300

    New post UI

---
+++ src/app/(protected)/(tabs)/index.tsx
@@ -0,0 +1,25 @@
+import { dummyPosts } from '@/dummyDate';
+import {  FlatList, Text, View } from "react-native";
+import PostListItem from '@/components/PostListitem';
+import { Link } from 'expo-router';
+import { blue } from 'react-native-reanimated/lib/typescript/Colors';
+
+
+export default function HomeScreen() {
+  return (
+    <FlatList 
+    data={dummyPosts} renderItem={({item}) => <PostListItem post={item}/>} 
+    ListHeaderComponent={() => (
+      <Link
+        href='/new'
+        className='p-4 text-3xl text-center text-blue-500'
+      >
+        new post
+      </Link>
+    )}
+    />
+   
+     
+  );
+}
+
